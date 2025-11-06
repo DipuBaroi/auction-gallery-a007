f7ef5493be48d4f68fb36679ad3a2ce3932d2200
DipuBaroi <dipubaroi758@gmail.com>
2025-11-06 12:26:52 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,14 @@ function App() {
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <main className='p-10 bg-blue-50 space-y-5'>
+        <h1 className='text-xl font-semibold'>Active Auction</h1>
+        <h3>Discover and bid on extraordinary items</h3>
+        <section className='flex gap-5'>
+          <div className='border-2 border-blue-500 w-3/4'></div>
+          <div className='border-2 border-amber-800 w-1/4'></div>
+        </section>
+      </main>
       <Footer></Footer>
       
     </>
